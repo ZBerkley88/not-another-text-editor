a43f670c52357e34a209226d538dc46c1c6a0200
Zack <zachary.berkley@gmail.com>
2023-02-04 16:50:23 -0500

    wrote scripts for package.json

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const routesFunction = require('./routes/htmlRoutes');
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+// add all of my routes to the express app
+routesFunction(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
